5867d90597cb69c3eef5c1e60662f908c1c32e3a
flashowner <a74327833@163.com>
2019-06-27 22:02:36 +0800

    添加用户发布的任务和用户接受的任务的界面

--- MiniProgram/pages/taskaccept/taskaccept.js
+++ MiniProgram/pages/taskaccept/taskaccept.js
@@ -5,7 +5,7 @@ Page({
    */
   data: {
     colors: ["yellow", "green", "grey", "red"],
-    pic_urls: ["../../images/general.png",'../../images/快递.png', '../../images/外卖.png', '../../images/资料.png', '../../images/问卷.png', '../../images/投简历.png', '../../images/组队请求.png', '../../images/其他.png'],
+    pic_urls: ["../../images/general.png", '../../images/快递.png', '../../images/外卖.png', '../../images/资料.png', '../../images/问卷.png', '../../images/投简历.png', '../../images/组队请求.png', '../../images/其他.png'],
     state_urls: ['../../images/yellow_circle.png', '../../images/green_circle.png', '../../images/gray_circle.png', '../../images/red_circle.png'],
     state_texts: ['正在进行', '已完成', '尚未开始', '超过时限'],
     coin_url: "../../images/money_icon.png",
@@ -44,90 +44,43 @@ Page({
     var that = this;
     var contents = [];
     var current = [];
-      wx.request({
-        url: 'http://172.26.17.164:8080/task/getAllTask', //仅为示例，并非真实的接口地址
-        header: {
-          'content-type': "application/x-www-form-urlencoded", // 默认值
-        },
-        success(res) {
-          contents = res.data.data.list;
-          //console.log(contents.length);
-          //console.log(contents[2]);
-          that.setData({ contentlist: contents });
-          var size = contents.length;
-          if (size < 10)
-            current = contents.slice(0, size);
-          else
-            current = contents.slice(0,10)
-          that.setData({currentlist: current});
-          that.setData({first: false});
-          that.setData({currentPage: current.length});
-        }
-      });
+    wx.request({
+      url: 'http://172.26.17.164:8080/task/getTaskByUsername', //仅为示例，并非真实的接口地址
+      header: {
+        'content-type': "application/x-www-form-urlencoded", // 默认值
+        'cookie': wx.getStorageSync('cookieKey')
+      },
+      success(res) {
+        contents = res.data.data.list;
+        //console.log(contents.length);
+        //console.log(contents[2]);
+        that.setData({
+          contentlist: contents
+        });
+        var size = contents.length;
+        if (size < 10)
+          current = contents.slice(0, size);
+        else
+          current = contents.slice(0, 10)
+        that.setData({
+          currentlist: current
+        });
+        that.setData({
+          first: false
+        });
+        that.setData({
+          currentPage: current.length
+        });
+      }
+    });
   },
 
   tapfunc: function (e) {
     var taskid = e.target.dataset.tid;
     var number = e.target.dataset.num;
-    //console.log(getApp().globalData.userInfo.isLogin);
-    if (getApp().globalData.userInfo.isLogin == true) {
-      wx.request({
-        url: 'http://172.26.17.164:8080/task/findAllUserInTask', //仅为示例，并非真实的接口地址
-        data: {
-          taskId: taskid
-        },
-        header: {
-          'content-type': "application/x-www-form-urlencoded", // 默认值
-          'cookie': wx.getStorageSync('cookieKey')
-        },
-        success(res) {
-          var userInfo = getApp().globalData.userInfo;
-          console.log(res.data.data.list[0].username);
-          console.log(userInfo);
-          var userlist = res.data.data.list;
-          var curnumber = res.data.data.list.length;
-          var haveuser = userlist.findIndex(function(s){
-            s.username == userInfo.username;
-          });
-          if (haveuser == -1) {
-            if (curnumber <= number) {
-              wx.navigateTo({
-                url: '../detail/detail?tid=' + taskid
-              });
-            } else {
-              wx.showToast({
-                title: '人数已满',
-                icon: 'none',
-                duration: 2000
-              })
-            }
-          } else {
-            wx.showToast({
-              title: '已领取',
-              icon: 'none',
-              duration: 2000
-            })
-          }
-        },
-        fail(err) {
-        }
-      });
-    } else {
-      wx.showModal({
-        title: '提示',
-        content: '请先登陆',
-        success(res) {
-          if (res.confirm) {
-            console.log('用户点击确定');
-            wx.navigateTo({
-              url: '../loginPage/loginPage'
-            });
-          } else if (res.cancel) {
-            console.log('用户点击取消');
-          }
-        }
-      })
-    }
+    wx.navigateTo({
+      url: '../acTaskDetail/acTaskDetail?tid=' + taskid
+    });
   },
 
   mask: function (number) {
@@ -167,15 +120,15 @@ Page({
       }
     });
   },
-  
-  onReachBottom: function() {
+
+  onReachBottom: function () {
     setTimeout(this.loadMore, 500);
   },
 
 
   loadMore: function () {
     var that = this;
-    
+
     var current = that.data.currentlist;
     var content = that.data.contentlist;
     var newlist = [];
@@ -190,10 +143,16 @@ Page({
       console.log(len);
       if (len - cur < 5) {
         current = current.concat(content.slice(cur, len));
-        that.setData({currentPage: len, currentlist: current});
+        that.setData({
+          currentPage: len,
+          currentlist: current
+        });
       } else {
-        current = current.concat(content.slice(cur, cur+5));
-        that.setData({ currentPage: cur+5, currentlist: current });
+        current = current.concat(content.slice(cur, cur + 5));
+        that.setData({
+          currentPage: cur + 5,
+          currentlist: current
+        });
       }
     }
   },
